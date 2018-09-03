028eb18a9a5e08f43489e54803084c98f105deea
Aiden Garipoli <aidosgaripoli@gmail.com>
2018-09-03 10:28:08 +1000

    add filtering, pagination and sorting

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,30 @@
-function feed (root, args, context, info) {
-  return context.db.query.links({}, info)
+async function feed (root, args, context, info) {
+  const where = args.filter ? {
+    OR: [
+      { url_contains: args.filter },
+      { description_contains: args.filter }
+    ]
+  } : {}
+
+  const queriedLinks = await context.db.query.links(
+    { where, skip: args.skip, first: args.first, orderBy: args.orderBy },
+    `{ id }`
+  )
+
+  const countSelectionSet = `
+    {
+      aggregate {
+        count
+      }
+    }
+  `
+
+  const linksSelection = await context.db.query.linksConnection({}, countSelectionSet)
+
+  return {
+    count: linksSelection.aggregate.count,
+    linksIds: queriedLinks.map(link => link.id)
+  }
 }
 
 module.exports = {
